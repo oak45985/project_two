c933c037da63156803bd58b37cfb7e2f3a2c485f
oak45985 <oak45985@gmail.com>
2022-09-03 19:31:26 -0500

    song routes worked thru

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -51,6 +51,76 @@ router.get('/:id', (req, res) => {
     });
 });
 
+//POST USER (CREATE A USER)
+router.post('/', (req, res) => {
+    User.create({
+        username: req.body.username,
+        email: req.body.email,
+        webpage: req.body.webpage,
+        password: req.body.password
+    })
+    .then(dbUserData => {
+        req.session.save(() => {
+            req.session.user_id = dbUserData.id;
+            req.session.username = dbUserData.name;
+            req.session.loggedIn = true;
+
+            res.json(dbUserData);
+        });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+router.post('/login', (req, res) => {
+    User.findOne({ 
+        where: {
+            email: req.body.email
+        }
+    }).then(dbUserData => {
+        if (!dbUserData) {
+            res.status(400).json({ message: "No user matches ID" });
+            return;
+        }
+        const checkPw = dbUserData.checkPassword(req.body.password);
+        if(!checkPw) {
+            res.status(400).json({ message: 'Password not valid' });
+            return;
+        }
+
+        req.session.save(() => {
+            req.session.user_id = dbUserData.id;
+            req.session.username = dbUserData.username;
+            req.session.loggedIn = true;
+
+            res.json({ user: dbUserData, messsage: "You're logged in!" });
+        });
+    });
+});
+
+//UPDATE USER
+router.put('/:id', (req, res) => {
+    User.update(req.body, {
+        individualHooks: true,
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbUserData => {
+        if(!dbUserData[0]) {
+            res.status(404).json({ message: "No user matches ID" });
+            return;
+        }
+        res.json(dbUserData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    })
+})
+
 //DELETE ONE USER
 router.delete('/:id', checkAuth, (req, res) => {
     User.destroy({
@@ -69,6 +139,17 @@ router.delete('/:id', checkAuth, (req, res) => {
         console.log(err);
         res.status(500).json(err);
     });
+});
+
+router.post('/logout', checkAuth, (req, res) => {
+    if(req.session.loggedIn) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    }
+    else {
+        res.status(404).end();
+    }
 });
 
 module.exports = router;
