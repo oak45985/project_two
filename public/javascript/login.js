6ff9fa2f7a4ff772a3e6e19d404d896360293f27
oak45985 <oak45985@gmail.com>
2022-09-05 21:21:03 -0500

    set up basic login items

---
+++ public/javascript/login.js
@@ -0,0 +1,54 @@
+async function liForm(event) {
+    event.preventDefault();
+
+    const email = document.querySelector('#email-li').value.trim();
+    const pw = document.querySelector('#pw-li').value.trim();
+
+    if (email && pw) {
+        const response = await fetch('/api/users/login', {
+            method: 'post',
+            body: JSON.stringify({
+                email,
+                password
+            }),
+            headers: { 'Content-Type': 'application/json' }
+        });
+
+        if (response.ok) {
+            document.location.replace('/profile');
+        } else {
+            alert(response.statusText);
+        }
+    }
+}
+
+async function suForm(event) {
+    event.preventDefault();
+    
+    const username = document.querySelector('#user-su').value.trim();
+    const email = document.querySelector('#email-su').value.trim();
+    const webpage = document.querySelector('#userwp-su').value.trim();
+    const pw = document.querySelector('#pw-su').value.trim();
+
+    if (username && email && pw) {
+        const response = await fetch('/api/users', {
+            method: 'post',
+            body: JSON.stringify({
+                username,
+                email,
+                webpage,
+                pw
+            }),
+            headers: { 'Content-Type': 'application/json' }
+        });
+
+        if (response.ok) {
+            console.log('User Created');
+        } else {
+            alert(response.statusText);
+        }
+    }
+}
+
+document.querySelector('.li-form').addEventListener('submit', liForm);
+document.querySelector('.su-form').addEventListener('submit', suForm);
