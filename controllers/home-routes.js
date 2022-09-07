3199a8ecbcd9e083d2e11a8ec3b899728dcd05bd
chiphvzvrd <christianipena@yahoo.com>
2022-09-06 21:02:45 -0500

    adding style and alignment

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -34,7 +34,7 @@ router.get('/', (req, res) => {
         res.render('homepage', {
             songs,
             loggedIn: req.session.loggedIn
-        });
+        })
     })
     .catch(err => {
         console.log(err);
