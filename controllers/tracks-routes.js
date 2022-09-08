e5663d29573b532488b26a74a8ddccf00cad989d
oak45985 <oak45985@gmail.com>
2022-09-08 11:03:50 -0500

    track to artist route rendered

--- controllers/tracks-routes.js
+++ controllers/tracks-routes.js
@@ -40,4 +40,46 @@ router.get('/', (req, res) => {
     });
 });
 
+router.get('/artists/:id', (req, res) => {
+    Artist.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: [
+            'id',
+            'artist_name',
+            'artist_webpage'
+        ],
+        include: [
+            {
+                model: Song,
+                attributes: ['id','title', 'bpm', 'key'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            // {
+            //     model: User,
+            //     attributes: ['id', 'username']
+            // }
+        ]
+    })
+    .then(dbArtistData => {
+        if (!dbArtistData) {
+            res.status(404).json({ message: "No artist matches ID" });
+            return;
+        }
+        const artist = dbArtistData.get({ plain: true });
+
+        res.render('single-artist', {
+            artist
+        });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
 module.exports = router;
