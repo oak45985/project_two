fce593d106773bc2397c5f7b4f5af95a147f7012
oak45985 <oak45985@gmail.com>
2022-09-08 18:11:21 -0500

    updated again

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const exphbs = require('express-handlebars');
 const routes = require('./controllers/');
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3007;
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
